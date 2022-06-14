62d128694ba09af47c518f534a6f0213711ae089
kaneera <ukaneera@Gmail.com>
2022-06-14 13:41:09 -0400

    is this working lmao

---
+++ ToDo/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDo
+//
+//  Created by Kaneera on 2022-06-14.
+//  Copyright © 2022 Kaneera. All rights reserved.
+//
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
